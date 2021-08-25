131e4bc9de0b5fb935ebceb13d488747d936a96a
Dheeraj Kadela <88156985+DheerKadela@users.noreply.github.com>
2021-08-25 13:41:48 +0530

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <title>Welcome To Journey</title>
+</head>
+<body>
+    <div id="navbar" style="font-style: Garamond;"><h3>Dheeraj Kadela</h3>
+    
+    <a href="about.php">Eduaction</a>
+    <a href="about.php">Current Work</a>
+   <a href="about.php">Skills</a>
+    <a href="about.php">About me</a>
+   <a href="about.php">Contact</a>
+</div>
+
+<div id="centerbar">
+<img id="dheer" src="dheera.jpg" alt="">
+<div id="rightbar">
+    <h5 id="your">Share Your Idea</h5>
+    <textarea id="userdata" type="text" name="user"></textarea>
+    
+</div>
+<input id="submit" style="margin-left: 85%;" type="submit">
+</div>
+<div id="para">
+    <p id="me">
+    Hey! It's Me Dheeraj From Rajasthan. Learner, IITian, Web Developer, Programmer, Coder, Problem Solver, Software Engineer, Energy Builder, Beast, Teacher, Maths Analyser, Data Analyser, Youtuber. Known Skills:-> C(basic), C++, PHP, PHP-Laravel, Javascript, HTML, CSS, Python, SQL, DBMS, PHPMysql, PHPMyadmin.
+
+    </p>
+    
+    
+</div>
+
+<div id="gellery" >
+    <h5 id="contectg">Contact Gallary</h5>
+    <img style="height: 1.5cm;" id="linked" src="download.png" alt="">
+    <img id="fb" src="fblogo.png" alt="">
+    <img id="insta" src="insta1.jpg" alt="">
+    <img id="github" src="github.png" alt="">
+    <img id="interview" src="croppedlogo.png" alt="">
+    
+    
+</div>
+<tr>
+<td><a id="link" href="https://www.linkedin.com/in/dheeraj-kadela-a05227196/">linkedin</a><td>
+<td><a id="face" href="https://www.facebook.com/dheerajkumar.karela">Facebook</a><td>
+<td><a id="insta2" href="https://www.instagram.com/vilen_by_way/">Instagram</a><td>
+<td><a id="github2" href="https://github.com/DheerKadela">Github</a><td> 
+<td><a id="inter2" href="https://www.interviewbit.com/profile/dheerajram52_13db35e072f6">InterviewBit</a><td>     
+</tr>
+<div id="bottom">
+
+</div>
+</body>
+</html>
